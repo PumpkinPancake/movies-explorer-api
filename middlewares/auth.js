71e02b533fb551123c5648ddea284435b7e314ae
Анастасия Маевская <Quri.mau@gmail.com>
2023-07-17 11:33:38 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,23 +6,15 @@ const UNAUTHORIZED_ERROR = require('../errors/unauthorizedError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UNAUTHORIZED_ERROR('Invalid or expired token');
-  }
-
   const token = authorization.replace('Bearer ', '');
-
+  let payload;
+  if (!authorization || !authorization.startsWith('Bearer ')) return next(new UNAUTHORIZED_ERROR('Invalid token'));
   try {
-    const payload = jwt.verify(token, JWT_SECRET);
-    req.user = payload;
-    next();
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    if (err instanceof jwt.JsonWebTokenError) {
-      return next(new UNAUTHORIZED_ERROR('Invalid token'));
-    }
-    next(err);
+    return next(new UNAUTHORIZED_ERROR('Invalid token'));
   }
+  req.user = payload;
   return next();
 };
 
